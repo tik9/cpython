248f133b4e0238d3345417c5a1805a518e82f56d
tik9 <len-win-user>
2020-12-04 21:07:04 +0100

    New Commit

--- image.py
+++ image.py
@@ -1,25 +1,38 @@
 from PIL import Image
 import os
 import pytesseract
-import sys
-import settings
 import re
+import settings
+import shutil
+import sys
 
 
-def prep():
-    counter = 1
-    with open(settings.mdDat, 'r', encoding="utf8") as f:
-        for line in f:
+splitter = 2
 
-            if '##' in line:
-                line = f"{line[6:]}?"
-            print('', line, counter, end='')
-            if counter == 10:
-                break
-            counter += 1
+
+def does_string_match(str_):
+    # mat = re.match(rf'unbenannt\.png\d{{splitter}}\.png$', str)
+    mat = re.match('unbenannt\.png\d{'+str(splitter)+'}\.png$', str_)
+    return mat is not None
+
+
+def cp():
+
+    for file in os.listdir(settings.pics):
+        full_file_name = os.path.join(settings.pics, file)
+        if does_string_match(file.lower()):
+            # unbenannt.png28.png
+            str = file.split('.')
+            str = str[1][len(str[1])-splitter:]
+            str = f'unbenannt{str}{settings.ftype}'
+            path = os.path.dirname(full_file_name)
+            file = os.path.join(path, str)
+            shutil.move(full_file_name, file)
+            print('file', file)
 
 
 def image():
+
     # print('ftype','pics','mddat',settings.ftype,settings.pics,settings.mdDat)
 
     # pytes = r'C:\program files\Tesseract-OCR\tesseract.exe'
@@ -35,8 +48,8 @@ def image():
             if file.lower().endswith(settings.ftype):
                 img = Image.open(file)
                 text = pytesseract.image_to_string(img)
-                # f.write(text)
-                print(file)
+                f.write(text)
+                # print(file)
                 # print(text)
 
 
@@ -45,7 +58,7 @@ def md():
     with open(settings.mdDat, 'r', encoding="utf8") as f:
         counter = 1
         str = ''
-        needles = ['CO', '', '�', 'oO', 'O', 'S', '©']
+        needles = ['CO', '', ' ', '�', '� ', 'oO', 'O', 'S', '©']
         needles_re = re.compile("|".join(map(re.escape, needles)))
         code = False
         for line in f:
@@ -59,7 +72,6 @@ def md():
             for m in needles_re.finditer(line):
                 # print(m.group(0))
                 line = line.replace(m.group(0), '')
-                # print('strchar', m.group(0),line)
 
             if line.startswith('?'):
                 line = line.replace('?', '')
@@ -67,13 +79,10 @@ def md():
                 counter += 1
                 continue
 
-            if '?' in line:
+            if '?' in line and not code:
                 str += f"\n\n#### {counter}. {line}"
                 counter += 1
                 continue
-
-            if 'Which one is not a JavaScript' in line:
-                print(line)
 
             if '```' in line and not code:
                 str += '\n'
@@ -92,9 +101,9 @@ def md():
             # if counter == 10:
                 # break
 
-        # print(str)
+        print(str)
 
-    with open(settings.mdDat, 'w') as f:
+    with open(settings.mdDat, 'w', encoding='utf8') as f:
         f.write(str)
 
 
@@ -139,6 +148,25 @@ def qa():
 
 
 settings.init()
-image()
+# cp()
+# image()
 # md()
 # qa()
+
+
+def prep():
+    str = ''
+    counter = 1
+    with open(settings.mdDat, 'r', encoding='utf8') as f:
+        for line in f:
+            if '##' in line:
+                str += f'\n\n{line}\n'
+                continue
+            str += line.replace('\n', '')
+
+            counter += 1
+        # print(str)
+    with open(settings.mdDat, 'w', encoding='utf8') as f: f.write(str)
+
+
+prep()
--- settings.py
+++ settings.py
@@ -4,13 +4,13 @@ import array as arr
 
 
 def init():
-    global answers, ftype, home, mdDat, mdFile, pics
+    global answers, ftype, home,mdDat, mdFile, pics
     
     answers = arr.array('i', [])
 
     ftype = '.png'
-    mdFile = 'react.md'
-    pic = 'pictures/bd'
+    mdFile = 'py_vb_proj_linux.md'
+    pic = 'pictures/bdpm'
     home = str(Path.home())
     mdDat = os.path.join(os.path.dirname(__file__), mdFile)
     pics = os.path.abspath(os.path.join(home, pic))
