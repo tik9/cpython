30426a1a95edd115abc56189e8a3464668c585c3
tiko <tik_win10>
2022-03-03 09:16:25 +0100

    file_op_add_to_content

--- file_ops.py
+++ file_ops.py
@@ -9,40 +9,47 @@ base_folder = os.path.dirname(os.path.dirname(__file__))
 # dir_path = os.getcwd()
 
 tik9 = os.path.join(base_folder, 'tik9.github.io')
-files = tik9
-# files = os.path.join(tik9, 'assets')
-# file_ = 'client.*'
-file_ = 'server.*'
-# file_ = 'radpidapi*'
+# files = tik9
+files = os.path.join(tik9, 'assets')
+file_ = 'client.js'
+clientjs=os.path.join(files,file_)
+# file_ = 'test/radpidapi*'
+# file_ = '*.ts'
 
 
 def main():
-    scriptfile, new_suffix=getfile()
-    rename(scriptfile,new_suffix)
+    file_op()
     # copy(scriptfile, new_suffix)
+
+
+def content_file():
+    new_elem='tools'
+    with open(clientjs, 'r') as f:
+        str = ''
+        for line in f:
+            if '//@ts' in line:
+                break
+            if ']' in line:
+                str+=f'"{new_elem}"'
+            str += line
+    return sorted(str)
+
+def file_op():
+    glob_file = glob.glob(os.path.join(files, file_))
+    scriptfile = ''
+    new_suffix = '.ts'
+    for filename in glob_file:
+        scriptfile = filename
+        p = Path(scriptfile)
+        if p.suffix == '.ts':
+            new_suffix = '.js'
+        p.rename(Path(p.parent, p.stem + new_suffix))
 
 def copy(scriptfile, new_suffix):
     p = Path(scriptfile)
-    dst=os.path.join(p.parent,p.stem+new_suffix)
+    dst = os.path.join(p.parent, p.stem+new_suffix)
 
-    shutil.copy(scriptfile,dst)
-
-def getfile():
-    glob_file = glob.glob(os.path.join(files, file_))
-    for filename in glob_file:
-        scriptfile = filename
-    
-    p = Path(scriptfile)
-    new_suffix = '.ts'
-    if p.suffix == '.ts':
-        new_suffix = '.js'
-    return scriptfile, new_suffix
-
-
-def rename(scriptfile, new_suffix):
-    p=Path(scriptfile)
-    p.rename(Path(p.parent, p.stem + new_suffix))
-
+    shutil.copy(scriptfile, dst)
 
 if __name__ == "__main__":
     main()
--- template.py
+++ template.py
@@ -1,6 +1,3 @@
-from http import server
-from importlib.resources import path
-from mimetypes import suffix_map
 import os
 from pathlib import Path
 import sys
@@ -9,37 +6,41 @@ home = str(Path.home())
 
 # print(home)
 
-tik9 = os.path.join(home, 'tik9.github.io')
-assets = os.path.join(tik9, 'assets')
+tik9 = os.path.join(home, 'repo')
+folder = os.path.join(tik9, 'fol')
 file_ = ''
+file_ = os.path.join(folder, file_)
 
 
 def main():
-    # print(loop_folder())
-    file_ = os.path.join(tik9, '.js')
-    
+    content = content_file()
+    print(content)
+
+
+def content_file():
+    new_elem=''
+    with open(file_, 'r') as f:
+        str = ''
+        for line in f:
+            str += line
+    return str
+
 
 def loop_folder():
     files = []
     for entry in os.scandir(assets):
-        if entry.name.endswith('.js'):
+        if entry.name.endswith('.ext'):
             p = Path(entry.name)
 
             files.append(f"{p.stem}{p.suffix}")
     return files
 
 
-def content(str):
-    with open(file_, 'r') as f:
-        for line in f:
-            str += line
-    return str
-
-
 def version():
-    python_ver=sys.version_info
-    major=sys.version_info.major
-    minor=sys.version_info.minor
+    python_ver = sys.version_info
+    major = sys.version_info.major
+    minor = sys.version_info.minor
+
 
 if __name__ == "__main__":
     main()
